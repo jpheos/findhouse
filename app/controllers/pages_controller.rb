499112580b2a08d5ab456e6b3ac9ecb7582a535b
djothefou <jooo.blanchard@gmail.com>
2018-08-21 11:34:33 +0200

    add some style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
 
-  before_action :set_stop_lyon, only: :home
-
   COLORS = {
    "0"    =>  "000000",
    "1"    =>  "c42525",
@@ -20,16 +18,18 @@ class PagesController < ApplicationController
   }
 
   def home
-    distance_min = 5
-    start_time = "07:30"
-    end_time  = "08:30"
 
-    stops = Stop.near("Lyon", 80)
+
+    distance_min = 5
+
+    stops = Stop.near("Lyon", 60)
     stops = stops.select {|s| s.distance > distance_min}
     @marker = []
+
+
     @markers = Gmaps4rails.build_markers(stops) do |stop, marker|
 
-      stats = stop.get_stats_schedule(@stop_lyon, start_time, end_time)
+      stats = stop.get_stats_schedule(stop_ids, start_time, end_time)
       nb_trains = stats.size
 
       color = nb_trains > 4 ? "more" : nb_trains.to_s
@@ -49,12 +49,16 @@ class PagesController < ApplicationController
 
   private
 
-  def set_stop_lyon
-    if params.has_key?("stop_ids")
-      @stop_lyon = params["stop_ids"]
-    else
-      @stop_lyon = ["StopPoint:OCETrain TER-87722025", "StopPoint:OCETrain TER-87723197", "StopPoint:OCETrain TER-87282624", "StopPoint:OCETrain TER-87721001"]
-    end
+  def start_time
+    @start_time ||= params[:time].try(:[], :start) || "07:30"
+  end
+
+  def end_time
+    @end_time ||= params[:time].try(:[], :end) || "08:30"
+  end
+
+  def stop_ids
+    @stop_ids ||= params[:stop_ids] || ["StopPoint:OCETrain TER-87722025", "StopPoint:OCETrain TER-87723197", "StopPoint:OCETrain TER-87282624", "StopPoint:OCETrain TER-87721001"]
   end
 
 end
